0f5b6018f7e0ba76fea2fb5f9fd3a82b7a698b08
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-11-02 18:13:12 +0100

    🟢: added `acconts` app and changed the default user model to `CustomUserModel`

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
 
     # Local apps
+    "accounts.apps.AccountsConfig",
     "snippets.apps.SnippetsConfig",
 ]
 
@@ -127,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
