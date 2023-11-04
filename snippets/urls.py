9f6b3f7ac3bd4c278c64da5155f9da79eaf749c4
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-11-04 14:18:51 +0100

    🟢: added view for the snippet highlight

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from .views import (
     api_root,
     SnippetList,
     SnippetDetail,
+    SnippetHighlight,
     UserList,
     UserDetail,
 )
@@ -13,6 +15,9 @@ urlpatterns = [
     path("", api_root, name="api_root"),
     path("snippets/", SnippetList.as_view(), name="snippet_list"),
     path("snippets/<int:pk>/", SnippetDetail.as_view(), name="snippet_detail"),
+    path("snippets/<int:pk>/highlight", SnippetHighlight.as_view(), name="snippet_highlight"),
     path("users/", UserList.as_view(), name="user_list"),
     path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
